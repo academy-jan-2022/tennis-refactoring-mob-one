f9319f0e0667affb1f14a48d40bde54ea8d2d6c5
sofia <sofia.carballo@codurance.com>
2022-02-11 14:31:50 +0100

    refactor

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,10 +1,11 @@
+import java.util.Objects;
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int scorePlayerOne = 0;
+    private int scorePlayerTwo = 0;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,64 +13,81 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (Objects.equals(playerName, player1Name))
+            scorePlayerOne += 1;
         else
-            m_score2 += 1;
+            scorePlayerTwo += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        if (scorePlayerOne == scorePlayerTwo)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            score = getScoreWhenAll();
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (scorePlayerOne >=4 || scorePlayerTwo >=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = getAdvantageOrWinner();
         }
         else
         {
-            for (int i=1; i<3; i++)
+            score = getRunningScore(score);
+        }
+        return score;
+    }
+
+    private String getRunningScore(String score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) tempScore = scorePlayerOne;
+            else { score +="-"; tempScore = scorePlayerTwo;}
+            switch(tempScore)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                case 0:
+                    score +="Love";
+                    break;
+                case 1:
+                    score +="Fifteen";
+                    break;
+                case 2:
+                    score +="Thirty";
+                    break;
+                case 3:
+                    score +="Forty";
+                    break;
             }
+        }
+        return score;
+    }
+
+    private String getAdvantageOrWinner() {
+        String score;
+        int minusResult = scorePlayerOne - scorePlayerTwo;
+        if (minusResult==1) score ="Advantage " + player1Name;
+        else if (minusResult ==-1) score ="Advantage " + player2Name;
+        else if (minusResult>=2) score = "Win for " + player1Name;
+        else score ="Win for " + player2Name;
+        return score;
+    }
+
+    private String getScoreWhenAll() {
+        String score;
+        switch (scorePlayerOne)
+        {
+            case 0:
+                    score = "Love-All";
+                break;
+            case 1:
+                    score = "Fifteen-All";
+                break;
+            case 2:
+                    score = "Thirty-All";
+                break;
+            default:
+                    score = "Deuce";
+                break;
+
         }
         return score;
     }
